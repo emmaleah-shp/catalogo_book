917304acefbc5d328d6af5eefc3b09bf35a6001f
unknown <ekjones017@gmail.com>
2023-06-29 16:48:53 -0400

    editing

--- R/before_script.R
+++ R/before_script.R
@@ -37,3 +37,59 @@ options(digits = 3)
 options(dplyr.print_min = 4, dplyr.print_max = 4)
 # Hide proj4 warnings:
 options("rgdal_show_exportToProj4_warnings" = "none")
+
+
+pacman::p_load(tidyselect, tidyverse, dplyr, glue, knitr, stringr, magrittr, 
+               sf, ggmap, ggplot2, flextable, data.table, sp, 
+               scales, devtools, tinytex, writexl, webshot, readr)
+# library(arcgisbinding)
+# arc.check_product()
+options(scipen=999)
+
+
+#whitespace
+wsp1<-function(x){
+        num<-str_count(x, "\\s+") - str_count(x, "\\w")
+        c<-sum(num>0)
+        c
+}
+
+whitespacedf<-function(df){
+        dfx<- df %>% as.data.frame() %>% dplyr::select(where(is.character))%>% na.exclude() %>%
+                dplyr::summarise(across(.cols = everything(), .fns = ~ wsp1(.x), .names = "{.col}"))
+        dff<-dfx %>% dplyr::summarise(across(.cols = everything(), .fns = ~ sum(.x!=0), .names = "{.col}")) %>% t()
+        dff<-as.data.frame(dff) %>% rownames_to_column()
+        names(dff)[c(2)]<-"whitespace"
+        dff
+}
+
+# unique 
+n_distinct<- function(x){
+        length(unique(x))
+}
+
+# Diagnosticar
+diagnosticar<- function(df){
+        dfx<-df %>% as.data.frame() %>% dplyr::select(!last_col()) # CAN also use select(!c("geometry"))
+        tip<-data.frame(sapply(dfx,typeof)) %>%
+                rownames_to_column()
+        vac<- dfx %>% dplyr::summarise(across(.cols = everything(), 
+                                              .fns = ~ sum(is.na(.x)), .names = "{.col}")) %>% t()
+        vac<- as.data.frame(vac) %>% rownames_to_column()
+        stch<- merge(tip,vac, by="rowname") %>% as.data.frame()
+        ws<-whitespacedf(dfx)
+        stch<-merge(stch, ws, by="rowname", all.x=TRUE)
+        uniq<- dfx %>% dplyr::summarise(across(everything(), .fns = ~ n_distinct(.x), .names ="{.col}")) %>% 
+                t() %>% as.data.frame() %>% rownames_to_column()
+        stch<-merge(stch, uniq, by="rowname")
+        stch[is.na(stch)] <- 0
+        stch<- stch %>% dplyr::mutate(pc_completo = (nrow(dfx) - (V1.x + whitespace)) / (nrow(dfx)) *100,
+                                      pc_repetido=(nrow(dfx) - V1.y) / (nrow(dfx)) *100)
+        names(stch)[c(2:5)]<-c("tipo","vacios","whitespace","n_unico")
+        stch
+}
+
+
+require(stringr)
+require(sf)
+path<-"E:/GDB_DATAMET.gdb"
